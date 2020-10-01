7713bf605f6e139b5b5076c74d14be050884f9eb
james mcmahon <jamesrmcmahon@yahoo.com>
2020-09-30 20:14:04 -0400

    first commit

---
+++ test/brick_test.exs
@@ -0,0 +1,15 @@
+defmodule BrickTest do
+  use ExUnit.Case
+
+  import Tetris.Brick
+
+
+
+
+  test "Creates a new brick" do
+    assert new_brick().name == :i
+  end
+
+  def new_brick, do: new()
+
+end
---
+++ test/tetris_test.exs
@@ -0,0 +1,8 @@
+defmodule TetrisTest do
+  use ExUnit.Case
+
+
+  test "greets the world" do
+    assert Tetris.hello() == :world
+  end
+end
